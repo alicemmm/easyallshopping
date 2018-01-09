a150cbe3582c8c3d3258b0de713521b057bdf42a
asia <guoyazhou@apkpure.com>
2018-01-09 18:16:52 +0800

    code

--- app/src/main/java/com/lomoasia/easyallshopping/MainActivity.java
+++ app/src/main/java/com/lomoasia/easyallshopping/MainActivity.java
@@ -1,24 +1,30 @@
 package com.lomoasia.easyallshopping;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.KeyEvent;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.just.agentweb.AgentWeb;
 import com.just.agentweb.ChromeClientCallbackManager;
+import com.just.agentweb.DefaultWebClient;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -28,6 +34,7 @@ public class MainActivity extends AppCompatActivity
 
     private LinearLayout mainLinearLayout;
     private AgentWeb agentWeb;
+    private TextView titleTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,18 +42,52 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         context = this;
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitleTextColor(Color.WHITE);
+        toolbar.setTitle("");
+        titleTextView = findViewById(R.id.toolbar_title);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setHomeButtonEnabled(true);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+
             }
         });
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        final DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.addDrawerListener(new DrawerLayout.DrawerListener() {
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+                View content = drawer.getChildAt(0);
+                View menu = drawerView;
+                float scale = 1 - slideOffset;
+
+                menu.setAlpha(0.6f + 0.4f * (1 - scale));
+                content.setTranslationX(menu.getMeasuredWidth() * (1 - scale));
+                content.setPivotX(0);
+                content.setPivotY(content.getMeasuredHeight() / 2);
+                content.invalidate();
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+
+            }
+
+            @Override
+            public void onDrawerStateChanged(int newState) {
+
+            }
+
+        });
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
@@ -58,28 +99,63 @@ public class MainActivity extends AppCompatActivity
         initView();
     }
 
-    private void initView(){
-        mainLinearLayout = findViewById(R.id.agentweb_webview_id);
-        agentWeb = AgentWeb.with(this)
-                .setAgentWebParent(mainLinearLayout, new LinearLayout.LayoutParams(-1, -1))
-                .useDefaultIndicator()
+    private void initView() {
+        mainLinearLayout = findViewById(R.id.main_layout_ll);
+        agentWeb = AgentWeb.with(this)//
+                .setAgentWebParent(mainLinearLayout, new LinearLayout.LayoutParams(-1, -1))//
+                .useDefaultIndicator()//
                 .defaultProgressBarColor()
-                .setReceivedTitleCallback(new ChromeClientCallbackManager.ReceivedTitleCallback() {
-                    @Override
-                    public void onReceivedTitle(WebView view, String title) {
-
-                    }
-                })
-                .createAgentWeb()
+                .setReceivedTitleCallback(mCallback)
+                .setWebChromeClient(mWebChromeClient)
+                .setWebViewClient(mWebViewClient)
+                .setMainFrameErrorView(R.layout.agentweb_error_page, -1)
+                .setSecurityType(AgentWeb.SecurityType.strict)
+                .setOpenOtherAppWays(DefaultWebClient.OpenOtherAppWays.ASK)//打开其他应用时，弹窗咨询用户是否前往其他应用
+                .interceptUnkownScheme() //拦截找不到相关页面的Scheme
+                .createAgentWeb()//
                 .ready()
-                .go("http://www.jd.com");
-
+                .go(getUrl());
     }
+
+
+    private WebViewClient mWebViewClient = new WebViewClient() {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+            return super.shouldOverrideUrlLoading(view, request);
+        }
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            //do you  work
+            Log.i("Info", "BaseWebActivity onPageStarted");
+        }
+    };
+    private WebChromeClient mWebChromeClient = new WebChromeClient() {
+        @Override
+        public void onProgressChanged(WebView view, int newProgress) {
+            //do you work
+//            Log.i("Info","progress:"+newProgress);
+        }
+    };
+
+    public String getUrl() {
+        return "https://m.jd.com/";
+    }
+
+    private ChromeClientCallbackManager.ReceivedTitleCallback mCallback = new ChromeClientCallbackManager.ReceivedTitleCallback() {
+        @Override
+        public void onReceivedTitle(WebView view, String title) {
+            if (titleTextView != null) {
+                titleTextView.setText(title);
+            }
+        }
+    };
+
 
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer!= null && drawer.isDrawerOpen(GravityCompat.START)) {
+        if (drawer != null && drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
             super.onBackPressed();
@@ -88,26 +164,32 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     protected void onPause() {
-        agentWeb.getWebLifeCycle().onPause();
+        if (agentWeb != null) {
+            agentWeb.getWebLifeCycle().onPause();
+        }
         super.onPause();
 
     }
 
     @Override
     protected void onResume() {
-        agentWeb.getWebLifeCycle().onResume();
+        if (agentWeb != null) {
+            agentWeb.getWebLifeCycle().onResume();
+        }
         super.onResume();
     }
 
     @Override
     protected void onDestroy() {
-        agentWeb.getWebLifeCycle().onDestroy();
+        if (agentWeb != null) {
+            agentWeb.getWebLifeCycle().onDestroy();
+        }
         super.onDestroy();
     }
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if (agentWeb.handleKeyEvent(keyCode, event)) {
+        if (agentWeb != null && agentWeb.handleKeyEvent(keyCode, event)) {
             return true;
         }
         return super.onKeyDown(keyCode, event);
